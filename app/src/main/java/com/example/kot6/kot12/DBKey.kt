0951cfb915e0cf89ccbdc168337579dd5f77da51
sophia <sheisinthezone@gmail.com>
2021-12-24 23:59:40 +0900

    kot12_implement_cardview_fun

---
+++ app/src/main/java/com/example/kot6/kot12/DBKey.kt
@@ -0,0 +1,10 @@
+package com.example.kot6.kot12
+
+class DBKey {
+    companion object {
+        const val USERS="Users"
+        const val LIKED_BY ="likedBy"
+        const val LIKE ="like"
+        const val DISLIKE ="dislike"
+    }
+}
