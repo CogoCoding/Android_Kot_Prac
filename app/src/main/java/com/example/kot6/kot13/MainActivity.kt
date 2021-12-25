9361b012e1ee545144e8e20cccd4eb823e34a2a1
sophia <sheisinthezone@gmail.com>
2021-12-25 23:09:12 +0900

    kot13_init_xml

--- app/src/main/java/com/example/kot6/kot13/MainActivity.kt
+++ app/src/main/java/com/example/kot6/kot13/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.kot6.kot13
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kot6.R
+import com.example.kot6.databinding.ActivityMainKot13Binding
 
 class MainActivity:AppCompatActivity() {
+    private lateinit var binding: ActivityMainKot13Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_kot12)
+//        setContentView(R.layout.activity_main_kot13)
+        binding = ActivityMainKot13Binding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
