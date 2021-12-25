6cdee183bd202f99a4a30c39a83d088c09c69480
sophia <sheisinthezone@gmail.com>
2021-12-25 23:06:01 +0900

    kot13_init

---
+++ app/src/main/java/com/example/kot6/kot13/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kot6.kot13
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kot6.R
+
+class MainActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_kot12)
+    }
+}
