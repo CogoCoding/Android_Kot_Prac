e0dff994dde3f1fe2b15800b5b41162ea7a13755
sophia <sheisinthezone@gmail.com>
2021-12-27 02:07:08 +0900

    kot13_implement_DB_Article

--- app/src/main/java/com/example/kot6/kot13/home/HomeFragment.kt
+++ app/src/main/java/com/example/kot6/kot13/home/HomeFragment.kt
@@ -1,7 +1,68 @@
 package com.example.kot6.kot13.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.kot6.R
+import com.example.kot6.databinding.FragmentHomeBinding
+import com.example.kot6.kot13.mypage.DBKey.Companion.DB_ARTICLES
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
+    private var binding : FragmentHomeBinding?=null
+    private lateinit var articleAdapter : ArticleAdapter
+    private val auth: FirebaseAuth by lazy{
+        Firebase.auth
+    }
+    private val articleList = mutableListOf<ArticleModel>()
+    private lateinit var articleDB: DatabaseReference
+    private val listener = object : ChildEventListener{
+        override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+            val articleModel = snapshot.getValue(ArticleModel::class.java)
+            articleModel?:return
+            articleList.add(articleModel)
+            articleAdapter.submitList(articleList)
+        }
+        override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+        override fun onChildRemoved(snapshot: DataSnapshot) {}
+        override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+        override fun onCancelled(error: DatabaseError) {}
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val fragmentHomeBinding = FragmentHomeBinding.bind(view)
+        binding = fragmentHomeBinding
+
+        articleList.clear()
+        articleDB = Firebase.database.reference.child(DB_ARTICLES)
+        articleAdapter = ArticleAdapter()
+        articleAdapter.submitList(mutableListOf<ArticleModel>().apply{
+            add(ArticleModel("0","이동천",100000,"1조", imageUrl = ""))
+            add(ArticleModel("0","이동천오뺘",200000,"1조", imageUrl = ""))
+        })
+
+        fragmentHomeBinding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
+        fragmentHomeBinding.articleRecyclerView.adapter = articleAdapter
+
+        articleDB.addChildEventListener(listener)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        articleAdapter.notifyDataSetChanged()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
 }
---
+++ app/src/main/java/com/example/kot6/kot13/mypage/DBKey.kt
@@ -0,0 +1,7 @@
+package com.example.kot6.kot13.mypage
+
+class DBKey {
+    companion object{
+        const val DB_ARTICLES = "Articles"
+    }
+}
